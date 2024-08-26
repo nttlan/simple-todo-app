0f8b8372142f9608c7fabca80736626fdfd29cf8
Thanh Lan <emeraldlan1105@gmail.com>
2024-08-26 16:05:12 +0700

    renew app & connect to mongodb

--- main.go
+++ main.go
@@ -2,200 +2,53 @@ package main
 
 import (
 	"os"
-	"log"
-	"net/http"
 
-	"github.com/joho/godotenv"
-	"gorm.io/gorm"
-	"gorm.io/driver/mysql"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var db *gorm.DB
-var err error
-
-// model
-type Task struct {
-	ID        uint      `json:"id" gorm:"unique, not null"`
-	Title     string    `json:"title" gorm:"not null"`
-	Status    string    `json:"status" gorm:"type:enum('Not started','Doing','Finished')"`
-	Details   string 	`json:"details"`
-}
-
-// always called, regardless of being called in main() or not
 func init() {
-	// load environment variables
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    err := godotenv.Load()
+    if err != nil {
+      log.Fatal("Error loading .env file")
+    }
 
-	// connect to mysql database
-	db_database := os.Getenv("DB_DATABASE")
-	db_username := os.Getenv("DB_USERNAME")
-	db_password := os.Getenv("DB_PASSWORD")
+    // Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+    serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+    opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI)
 
-	dsn := db_username + ":" + db_password + "@tcp(127.0.0.1:3306)/" + db_database + "?charset=utf8&parseTime=True&loc=Local"
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalln("Cannot connect to MySQL:", err)
-	} else {
-		log.Println("Connected to MySQL:", db)
-	}
+    // Create a new client and connect to the server
+    client, err := mongo.Connect(context.TODO(), opts)
+    if err != nil {
+        panic(err)
+    }
 
-	db.AutoMigrate(&Task{})
+    defer func() {
+        if err = client.Disconnect(context.TODO()); err != nil {
+          panic(err)
+        }
+    }()
+
+    // Send a ping to confirm a successful connection
+    err = client.Ping(context.TODO(), readpref.Primary())
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
 
 func main() {
-	// create a gin router with default middleware
 	router := gin.Default()
+    fmt.Println("Hello, World!")
 
-	router.GET("/tasks", getTasksIndex)
-	router.POST("/tasks", createTask)
-	router.GET("/tasks/:id", getSingleTask)
-	router.PUT("/tasks/:id", updateSingleTask)
-	router.DELETE("tasks/:id", deleteSingleTask)
-
-	// by default it serves on :8080 unless a PORT environment variable was defined
-	router.Run()
-}
-
-func getTasksIndex(c *gin.Context) {
-	// get the tasks
-	var tasks []Task
-	err = db.Find(&tasks).Error
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// response
-	c.JSON(http.StatusOK, gin.H{
-		"data": tasks,
-	})	
-}
-
-
-func createTask(c *gin.Context) {
-	// get data from request body
-	var body struct {
-		Title    string `json:"title" binding:"required"`
-		Status   string `json:"status" binding:"required"`
-		Details  string `json:"details" binding:"required"`
-	}
-
-	err = c.ShouldBindJSON(&body)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// create a task
-	task := Task{
-		Title: body.Title,
-		Status: body.Status,
-		Details: body.Details,
-	}
-	err = db.Create(&task).Error
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// response
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Created successfully",
-	})
-}
-
-func getSingleTask(c *gin.Context) {
-	// get id from url
-	id := c.Param("id")
-
-	var task Task
-	err = db.First(&task, id).Error
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// response
-	c.JSON(http.StatusOK, gin.H{
-		"data": task,
-	})
-}
-
-func updateSingleTask(c *gin.Context) {
-	// get id from url
-	id := c.Param("id")
-
-	// get data from request body
-	var body struct {
-		Title    string `json:"title" binding:"required"`
-		Status   string `json:"status" binding:"required"`
-		Details  string `json:"details" binding:"required"`
-	}
-
-	err = c.ShouldBindJSON(&body)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// find and update the task
-	var task Task
-	err = db.First(&task, id).Error
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	err = db.Model(&task).Updates(Task{
-		Title: body.Title,
-		Status: body.Status,
-		Details: body.Details,
-		}).Error
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// response
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Updated successfully",
-	})
-}
-
-func deleteSingleTask(c *gin.Context) {
-	// get id from url
-	id := c.Param("id")
-
-
-	// delete the tasks
-	err = db.Delete(&Task{}, id).Error
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// response
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Deleted successfully",
-	})
+	router.Run(":8080")
 }
