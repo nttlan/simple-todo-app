f42e1cc4dd61a5174c6b87b97a6e9df925554ce5
Thanh Lan <emeraldlan1105@gmail.com>
2024-08-26 22:46:22 +0700

    create db config

---
+++ config/db.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"os"
+	"github.com/joho/godotenv"
+	"context"
+	"log"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func InitDB() *mongo.Client {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		log.Fatal("MONGO_URI is not set")
+	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	
+	return client
+}
+
+func DisconnectDB(client *mongo.Client) {
+	if err := client.Disconnect(context.TODO()); err != nil {
+		log.Fatal("Error disconnecting from MongoDB:", err)
+	} else {
+		log.Println("Disconnected from MongoDB successfully")
+	}
+}
--- main.go
+++ main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"os"
-
+    "log"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-
-	"context"
-	"fmt"
-	"log"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+    "github.com/emerald-lan/simple-todo-app/config"
 )
 
-func init() {
-    err := godotenv.Load()
-    if err != nil {
-      log.Fatal("Error loading .env file")
-    }
-
-    // Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-    serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-    opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI)
-
-    // Create a new client and connect to the server
-    client, err := mongo.Connect(context.TODO(), opts)
-    if err != nil {
-        panic(err)
-    }
-
-    defer func() {
-        if err = client.Disconnect(context.TODO()); err != nil {
-          panic(err)
-        }
-    }()
-
-    // Send a ping to confirm a successful connection
-    err = client.Ping(context.TODO(), readpref.Primary())
-    if err != nil {
-        panic(err)
-    }
-
-    fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-}
-
 func main() {
+    db := config.InitDB()
+    defer config.DisconnectDB(db)
+
 	router := gin.Default()
-    fmt.Println("Hello, World!")
+    log.Println("Hello, World!")
 
 	router.Run(":8080")
 }
