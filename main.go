0a9616f573bdcbff11404468dc3fc2e6ac98b035
Thanh Lan <emeraldlan1105@gmail.com>
2024-08-27 10:33:18 +0700

    update main

--- main.go
+++ main.go
@@ -4,14 +4,25 @@ import (
     "log"
 	"github.com/gin-gonic/gin"
     "github.com/emerald-lan/simple-todo-app/config"
+    "github.com/emerald-lan/simple-todo-app/controllers"
+    "github.com/emerald-lan/simple-todo-app/services"
 )
 
 func main() {
-    db := config.InitDB()
-    defer config.DisconnectDB(db)
+    client := config.InitDB()
+    defer config.DisconnectDB(client)
+
+    db := client.Database("simple-todo-app")
+
+    taskService := services.NewTaskService(db)
+    
+    taskController := controllers.NewTaskController(taskService)
 
 	router := gin.Default()
-    log.Println("Hello, World!")
+    log.Println("Server started on port 8080")
+
+    router.POST("/tasks", taskController.CreateTask)
+    router.GET("/tasks", taskController.GetTasks)
 
 	router.Run(":8080")
 }
